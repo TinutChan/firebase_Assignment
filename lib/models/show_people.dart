d6f80eaa9a8c184b2612efee3f4f496579a63502
TinutChan <tinutchan444@gmail.com>
2022-07-12 14:18:25 +0700

    updated

--- lib/models/show_people.dart
+++ lib/models/show_people.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_assignment/models/people_model.dart';
 import 'package:firebase_assignment/pages/create_people_page.dart';
 import 'package:firebase_assignment/pages/detail_people_page.dart';
+import 'package:firebase_assignment/screens/home_screen.dart';
 import 'package:firebase_assignment/services/people_service.dart';
 import 'package:flutter/material.dart';
 
@@ -10,53 +11,54 @@ class ShowPeoplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<List<PeopleModel>>(
-        stream: PeopleService().getPeople(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return ListView.separated(
-            itemBuilder: (context, index) {
-              var data = snapshot.data![index];
-              return Card(
-                child: ListTile(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => DetailPeoplePage(people: data),
+      body: Container(
+        color: Colors.grey.shade200,
+        child: RefreshIndicator(
+          onRefresh: () {
+            return Future.value();
+          },
+          child: StreamBuilder<List<PeopleModel>>(
+            stream: PeopleService().getPeople(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(child: Text('Something went wrong'));
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  var data = snapshot.data![index];
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => DetailPeoplePage(people: data),
+                          ),
+                        );
+                      },
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          data.photo ??
+                              'https://3znvnpy5ek52a26m01me9p1t-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/noimage_person.png',
+                        ),
                       ),
-                    );
-                  },
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      data.photo ??
-                          'https://3znvnpy5ek52a26m01me9p1t-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/noimage_person.png',
+                      title: Text(data.name!),
+                      subtitle: Text(data.numphone!),
+                      trailing: Text(data.gender!),
                     ),
-                  ),
-                  title: Text(data.name!),
-                  subtitle: Text(data.numphone!),
-                  trailing: Text(data.gender!),
-                ),
+                  );
+                },
+                separatorBuilder: (context, index) => const Divider(),
+                itemCount: snapshot.data!.length,
               );
             },
-            separatorBuilder: (context, index) => const Divider(),
-            itemCount: snapshot.data!.length,
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => const CreatePeoplePage()),
-          );
-        },
-        child: const Icon(Icons.add),
+          ),
+        ),
       ),
     );
   }
